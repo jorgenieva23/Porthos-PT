da32c357c821f5975dcff6b5b346e78d66a85225
jorgenieva23 <jorge_4755@hotmail.com>
2025-08-20 20:18:12 -0300

    config: conexión a postgres con DATABASE_URL

--- api/src/db.js
+++ api/src/db.js
@@ -5,16 +5,15 @@ dotenv.config();
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-// Verifica que las variables de entorno estén presentes
 if (!DB_USER || !DB_PASSWORD || !DB_HOST || !DB_NAME) {
   throw new Error("Faltan variables de entorno de la base de datos");
 }
 
-// Instancia de Sequelize para conectar a la base de datos PostgreSQL
-export const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-  {
-    logging: false,
-    dialect: "postgres",
-  }
-);
+const isProduction = process.env.NODE_ENV === "production";
+
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+});
+
